e2c9c4948f161f0bc944e42213dde76ef399cb9f
nqv2110 <73152126+vietmat12333@users.noreply.github.com>
2021-11-03 16:03:23 +0700

    fisrt commit

---
+++ models/product.php
@@ -0,0 +1,61 @@
+<?php class Product extends Db
+{
+    public function getAllProducts()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+
+    }
+    public function getProductById($id)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE id = ?");
+        $sql->bind_param("i", $id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    function getNew10Product()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM  products ORDER BY `id` DESC LIMIT 10");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    function getProductLapTop(){
+        $sql = self::$connection->prepare("SELECT * FROM `products` WHERE `type_id`=3");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    function getProductPhones(){
+        $sql = self::$connection->prepare("SELECT * FROM `products` WHERE `type_id`=1");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    function getProductIpad(){
+        $sql = self::$connection->prepare("SELECT * FROM `products` WHERE `type_id`=4");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    function search($keyword){
+        $sql = self::$connection->prepare("SELECT * FROM `products` WHERE `name` Like ?");
+        $keyword="%$keyword%";
+        $sql ->bind_param('s',$keyword);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+  
+
+}
